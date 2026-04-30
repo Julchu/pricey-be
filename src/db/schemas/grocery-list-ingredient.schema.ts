d1cfefc7c0720a9f70669c44095a4a8ba3184a14
julchu <jchumtl@gmail.com>
2026-04-29 21:52:09 -0400

    Removing TODO

--- src/db/schemas/grocery-list-ingredient.schema.ts
+++ src/db/schemas/grocery-list-ingredient.schema.ts
@@ -1,21 +1,9 @@
-import {
-  integer,
-  numeric,
-  pgTable,
-  unique,
-  varchar,
-} from "drizzle-orm/pg-core";
-import {
-  type AutomaticFields,
-  type PrivateFields,
-  requiredColumns,
-  timestamps,
-} from "../utils/shared-schema";
+import { integer, numeric, pgTable, unique, varchar, } from "drizzle-orm/pg-core";
+import { type AutomaticFields, type PrivateFields, requiredColumns, timestamps, } from "../utils/shared-schema";
 import { type InferInsertModel, type InferSelectModel } from "drizzle-orm";
 import { groceryListTable } from "./grocery-list.schema";
 import { ingredientTable, unitEnum } from "./ingredient.schema"; // TODO: frontend dropdown to select similar ingredient from master list to get price (and set ingredientId)
 
-// TODO: frontend dropdown to select similar ingredient from master list to get price (and set ingredientId)
 export const groceryListIngredientTable = pgTable(
   "grocery_list_ingredients",
   {
